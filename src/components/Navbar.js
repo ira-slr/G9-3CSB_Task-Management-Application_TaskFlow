c175bc72aeee9a9fcf926fd9cf57feddf8cf5ebe
Cheemsburb <sencilgladwyn@gmail.com>
2025-10-19 23:29:20 -0700

    testing my ssh again

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-// Sencil, Gladwynigt
+// Sencil, Gladwynigttt
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
